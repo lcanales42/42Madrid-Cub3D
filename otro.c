8c349f8f58e545c4d5c8b4ec7271b660681d533f
lorenzo <lcanales@student.42madrid.com>
2020-11-09 23:38:16 +0100

    prueba

--- otro.c
+++ otro.c
@@ -6,17 +6,17 @@
 /*   By: lcanales <lcanales@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/09 23:18:57 by lcanales          #+#    #+#             */
-/*   Updated: 2020/11/09 23:20:17 by lcanales         ###   ########.fr       */
+/*   Updated: 2020/11/09 23:36:32 by lcanales         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-nclude <mlx.h>
+#include <mlx.h>
 
 int main(void)
 {
 	void *mlx;
 	void *window;
-
+	
 	mlx = mlx_init();
 	window = mlx_new_window(mlx, 1000, 1000, "Title");
 
