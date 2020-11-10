1ff47ac273ce3c261cb29008ca4fce94e7e63c3f
lorenzo <lcanales@student.42madrid.com>
2020-11-10 19:41:47 +0100

    ahora

--- otro.c
+++ otro.c
@@ -6,7 +6,7 @@
 /*   By: lcanales <lcanales@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/09 23:18:57 by lcanales          #+#    #+#             */
-/*   Updated: 2020/11/10 09:30:43 by lcanales         ###   ########.fr       */
+/*   Updated: 2020/11/10 19:39:33 by lcanales         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,17 @@ int main(void)
 	window2 = mlx_new_window(mlx, 500, 500, "Hola Mundo");
 //	mlx_destroy_window(mlx, window);
 	mlx_clear_window(mlx, window);
-	window = mlx_new_window(mlx, 100, 100, "Esto");
+	//window = mlx_new_window(mlx, 100, 100, "Esto");
+	mlx_pixel_put(mlx, window, 500, 500, 125|012|122|255);
+	mlx_pixel_put(mlx, window, 501, 500, 125|012|122|255);
+	mlx_pixel_put(mlx, window, 502, 500, 125|012|122|255);
+	mlx_pixel_put(mlx, window, 503, 500, 125|012|122|255);
+	mlx_pixel_put(mlx, window, 504, 500, 125|012|122|255);
+	mlx_pixel_put(mlx, window, 505, 500, 125|012|122|255);
+	mlx_pixel_put(mlx, window, 506, 500, 125|012|122|255);
+	mlx_pixel_put(mlx, window, 507, 500, 125|012|122|255);
+	mlx_string_put(mlx,window2, 250, 250, 125|012|122|255, "hola");	
+
 
 	mlx_loop(mlx);
 	return (0);
