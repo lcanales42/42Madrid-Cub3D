e504fbf2625dfeaae84732313f5e99ec629733b9
lcanales42 <lcanales@student.42madrid.com>
2020-11-10 10:55:14 +0100

    otro

--- otro.c
+++ otro.c
@@ -6,7 +6,7 @@
 /*   By: lcanales <lcanales@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/09 23:18:57 by lcanales          #+#    #+#             */
-/*   Updated: 2020/11/09 23:36:32 by lcanales         ###   ########.fr       */
+/*   Updated: 2020/11/10 09:30:43 by lcanales         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,14 @@ int main(void)
 {
 	void *mlx;
 	void *window;
+	void *window2;	
 	
 	mlx = mlx_init();
 	window = mlx_new_window(mlx, 1000, 1000, "Title");
+	window2 = mlx_new_window(mlx, 500, 500, "Hola Mundo");
+//	mlx_destroy_window(mlx, window);
+	mlx_clear_window(mlx, window);
+	window = mlx_new_window(mlx, 100, 100, "Esto");
 
 	mlx_loop(mlx);
 	return (0);
